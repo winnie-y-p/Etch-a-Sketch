0d20e443bc71051d9a8eefa1e27420f118ae2f61
Winnie Poon <winnie.y.poon@gmail.com>
2025-05-18 02:10:49 +1000

    Add color choice

--- easScript.js
+++ easScript.js
@@ -1,7 +1,12 @@
 const grid = document.querySelector(".grid");
 const generateBtn = document.querySelector("#generate");
 
+//Default
+document.addEventListener("DOMContentLoaded", () => {
+    generateGrid(16);
+})
 
+//Making the grid
 function generateGrid(numberInput) {
     for (let i = 0; i < numberInput * numberInput; i++) {
         const square = document.createElement("div");
@@ -9,7 +14,7 @@ function generateGrid(numberInput) {
         square.style.width = `calc(100%/${numberInput})`;
         square.style.height = `calc(100%/${numberInput})`;
         square.addEventListener("mouseenter", () => {
-            square.style.backgroundColor = "white";
+            square.style.backgroundColor = `${colorChoice.value}`;
         })
         grid.appendChild(square);
     }
@@ -20,9 +25,6 @@ function removeGrid () {
     squares.forEach(square => square.remove());
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    generateGrid(16);
-})
 generateBtn.addEventListener("click", () => {
     removeGrid();
     const userResponse = prompt("From 1-100, how detailed do your want your drawing? (1 being the least and 100 the most)", "16");
@@ -35,6 +37,7 @@ Please enter a number between 1 and 100.`)
     }
 });
 
+//Reset Grid
 function resetGrid () {
     removeGrid();
     generateGrid(16);
@@ -43,4 +46,11 @@ function resetGrid () {
 const reset = document.querySelector("#reset");
 reset.addEventListener("click", resetGrid);
 
+//option buttons
+
+const colorBtn = document.querySelector("#color-btn");
+const randomBtn = document.querySelector("#random-btn");
+const hueBtn = document.querySelector("#hue-btn");
+const colorChoice = document.querySelector("#colorChoice");
+const squareClass = document.querySelectorAll(".square");
 
